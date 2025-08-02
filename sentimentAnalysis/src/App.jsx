2aaf08bed764c4d23fe4307aff54ac5eec65e39a
Sami <samsal6686@gmail.com>
2025-08-02 15:18:10 -0500

    react app

---
+++ sentimentAnalysis/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Header from './Header.jsx'
+
+function App() {
+
+  return (
+    <>
+      <head>
+        <title>Stock Sentiment Analyzer</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet"/>
+      </head>
+      <Header/>
+      <div className="container">
+        <h1>BELLO</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ sentimentAnalysis/src/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+function Header() {
+
+  return (
+    <>
+        <div class="header">
+            <h1 class="text-center mb-0">📈 Stock Sentiment Analyzer</h1>
+            <p class="text-center mt-2 mb-0">Real-time sentiment analysis for trending stocks (e.g. FAANG)</p>
+        </div>
+    </>
+  )
+}
+
+export default Header
